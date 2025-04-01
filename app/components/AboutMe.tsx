767c42045cea004091f6f18a3cd9cfcd9847d4b9
Marianoromero8 <marianoromero97.mr@gmail.com>
2025-04-01 01:06:48 -0300

    fix: navbar width

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -16,19 +16,19 @@ export default function AboutMe() {
             </div>
             <div className='text-[#0094C6] flex flex-col gap-6 lg:gap-8 text-center lg:text-left pt-8'>
                 <p className="text-sm sm:text-base">
-                    I'm a passionate Frontend Developer from Bahía Blanca, Argentina, currently in my final year of a two-year University Degree in Programming at UTN, with 2 years of experience specializing in building dynamic, scalable, and visually appealing web applications.
+                    I'm a passionate Frontend Developer from Bahía Blanca, Argentina, currently in my final year of a two-year University Degree in Programming at UTN, with 2 years of experience specializing in building dynamic, scalable and visually appealing web applications.
                 </p>
                 <p className="text-sm sm:text-base">
-                    My expertise includes React.js, Next.js, TypeScript, Redux Toolkit, JavaScript, Firebase, and Stripe, among other technologies. I primarily use Tailwind CSS for styling, ensuring efficient and maintainable UI development.
+                    My expertise includes React.js, Next.js, TypeScript, Javascript, Redux Toolkit, Firebase and Stripe, among other technologies. I primarily use Tailwind CSS for styling, ensuring efficient and maintainable UI development.
                 </p>
                 <p className="text-sm sm:text-base">
-                    I worked as a Frontend Developer on an e-commerce project, focusing on creating a seamless user experience, integrating secure payment gateways and developing reusable UI components. Beyond that, I have also built API-driven applications and interactive web experiences, always striving for clean, efficient, and high-performance code.
+                    I worked as a freelance Frontend Developer for a local distributor, creating a Single Page Application (SPA) to showcase their product catalog, improving their online presence and accessibility. Additionally, I developed an e-commerce project where I focused on enhancing the user experience, integrating secure payment gateways, implementing third-party authentication and building reusable UI components.
                 </p>
                 <p className="text-sm sm:text-base">
-                    I’m a proactive, self-taught, and team-oriented developer with a strong ability to adapt and learn quickly. I enjoy collaborating with others, solving problems efficiently, and bringing creative solutions to challenges. I thrive in team environments, where communication and cooperation are key, and I’m always eager to learn from my peers. My goal is to create seamless user experiences while staying up to date with the latest frontend trends and best practices.
+                    I have also built API-driven applications and interactive web experiences, always striving for clean, efficient and high-performance code. I’m a proactive, self-taught and team-oriented developer with a strong ability to adapt and learn quickly. I enjoy collaborating with others, solving problems efficiently and bringing creative solutions to challenges.
                 </p>
                 <p className="text-sm sm:text-base">
-                    I aim to not only contribute my skills but also grow and evolve within the company I join, taking on new challenges and responsibilities that allow me to continuously improve as a developer. My goal is to create seamless, engaging user experiences while staying up to date with the latest frontend trends and best practices.
+                    My goal is to contribute my skills, grow within a company and take on new challenges that allow me to continuously improve as a developer while delivering seamless and engaging user experiences.
                 </p>
                 <h2 className='flex justify-center text-2xl sm:text-3xl hover:scale-110 transition duration-500'>
                     <a href="mailto:marianoromero97.mr@gmail.com">
